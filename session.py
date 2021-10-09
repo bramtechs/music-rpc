cb5ad4e97e4b3a78155254939c7aea2412267712
brambasiel <brambasiel@telenet.be>
2021-10-09 21:53:55 +0200

    support for audio streams

--- session.py
+++ session.py
@@ -75,33 +75,49 @@ class Session:
     def get_player_state(self) -> str:
         return utils.execute(self.cmd_prefix + "status")
 
-    def get_end_time(self) -> str:
-        position = int(utils.execute(self.cmd_prefix+"metadata --format \"{{position}}\""))*0.000001
-        length = int(utils.execute(self.cmd_prefix+"metadata --format \"{{mpris:length}}\""))*0.000001
-        return time.time()+length-position
+    def get_length(self) -> int:
+        length_text = utils.execute(self.cmd_prefix+"metadata --format \"{{mpris:length}}\"")
+        if (length_text != ""): # if not listening to icecast livestream (length not null)
+            return int(length_text)*0.000001
+        return -1 # unlimited length
+
+    def get_position(self) -> int:
+        return int(utils.execute(self.cmd_prefix+"metadata --format \"{{position}}\""))*0.000001
 
     def refresh(self,config):
         self.side = not self.side
         details = self.get_song_details()
         state = self.get_song_state()
         player_state = self.get_player_state()
+        end_time = -1
         if player_state == "Playing":
-            end_time = self.get_end_time()
+            if self.get_length() != -1:
+                end_time = time.time()+self.get_length()-self.get_position() # get end time
         elif player_state == "No players found" or player_state == '':
             self.running = False
             return
-        else:
-            end_time = 0
         large_image = config["logo"]
         small_image = config[player_state]
-        discord_rpc.update_presence(
-            **{
-                'state': state,
-                'details': details,
-                'end_timestamp': end_time,
-                'large_image_key': large_image,
-                'small_image_key': small_image
-            })
+        if end_time > 0:
+            discord_rpc.update_presence(
+                **{
+                    'state': state,
+                    'details': details,
+                    'start_timestamp':0,
+                    'end_timestamp': end_time,
+                    'large_image_key': large_image,
+                    'small_image_key': small_image
+                })
+        else:
+            discord_rpc.update_presence(
+                **{
+                    'state': state,
+                    'details': details,
+                    'start_timestamp': time.time()-self.get_position(),
+                    'end_timestamp':0,
+                    'large_image_key': large_image,
+                    'small_image_key': small_image
+                })
         discord_rpc.update_connection()
         time.sleep(2)
         discord_rpc.run_callbacks()
